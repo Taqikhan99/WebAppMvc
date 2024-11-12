2fd6d1f4d1394b0d751a2700567ed0a2da2591df
Taqi.Ahmed <Taqi.Ahmed@CENTEGYDEV04>
2024-11-12 11:06:20 +0500

    created new dt layer proj

---
+++ DataLayer/DbClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataLayer
+{
+    public class DbClass
+    {
+        string constr;
+        SqlConnection con;
+        public DbClass()
+        {
+            constr = ConfigurationManager.ConnectionStrings["dbConn"].ToString();
+            con = new SqlConnection(constr);
+        }
+
+        //procedure call for insert
+        public bool execInsertProc(string procname, List<SqlParameter> sqlParameters)
+        {
+            int i = 0;
+
+            using (SqlCommand cmd = new SqlCommand(procname, con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (sqlParameters != null)
+                {
+                    foreach (SqlParameter p in sqlParameters)
+                    {
+                        cmd.Parameters.Add(p);
+                    }
+                }
+
+                con.Open();
+                i = cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            if (i >= 1)
+            {
+                return true;
+            }
+            return false;
+
+
+
+        }
+
+        //make generic function to call get procedures
+        public DataTable execGetProc(string procname, List<SqlParameter> parameters = null)
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand(procname, con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                {
+                    foreach (SqlParameter p in parameters)
+                    {
+                        cmd.Parameters.Add(p);
+                    }
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                con.Open();
+                adapter.Fill(dt);
+                con.Close();
+
+
+            }
+
+            return dt;
+
+        }
+
+        public DataTable execQuery(string query)
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                con.Open();
+                adapter.Fill(dt);
+                con.Close();
+
+
+            }
+
+            return dt;
+
+        }
+    }
+}
--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using DataLayer;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DbClass dbClass;
+        public HomeController()
+        {
+            dbClass = new DbClass();
+        }
+       
+
         public ActionResult Index()
         {
+            DataTable dt= dbClass.execQuery("Select * from AspNetUsers");
+
             ViewBag.Message = "Congratulations for new home";
+            ViewBag.DataTable = dt;
             return View();
         }
 
